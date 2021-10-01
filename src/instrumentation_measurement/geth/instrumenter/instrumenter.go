df2496da189fccf892582af504e6f4c924423452
pdobacz <5735525+pdobacz@users.noreply.github.com>
2021-10-01 09:26:21 +0200

    Gather and analyze timer overheads (#44)
    * rough cut: measure geth timer overhead
    
    * refine, add new notebook
    
    * explore just one program, draw some initial geth-conclusions
    
    * use brutal instrumenter from go-ethereum branch
    
    * cherrypick: fixup notebook
    
    * use superbigimage class for big plots
    
    * generalize to geth and geth_brutal pt 1
    
    * generalize to geth and geth_brutal pt 2
    
    * allow evmone to handle timer overhead
    
    * add evmone to the notebook
    
    * add openethereum to notebook
    
    * update evmone and openethereum submodules
    
    * add a histogram for measurements with runmin timer subtracted
    
    * fix naming after review

--- src/instrumentation_measurement/geth/instrumenter/instrumenter.go
+++ src/instrumentation_measurement/geth/instrumenter/instrumenter.go
@@ -20,17 +20,23 @@ type LogConfig struct {
 // InstrumenterLog is emitted to the vm.EVM each cycle and lists information about the current internal state
 // prior to the execution of the statement.
 type InstrumenterLog struct {
-	Pc     uint64    `json:"pc"`
-	Op     vm.OpCode `json:"op"`
-	TimeNs int64     `json:"timeNs"`
+	Pc          uint64    `json:"pc"`
+	Op          vm.OpCode `json:"op"`
+	TimeNs      int64     `json:"timeNs"`
+	TimerTimeNs int64     `json:"timerTimeNs"`
 }
 
 // InstrumenterLogger is an vm.EVM state logger and implements Tracer.
 type InstrumenterLogger struct {
 	cfg LogConfig
 
-	logs            []InstrumenterLog
-	lastCaptureTime int64
+	logs      []InstrumenterLog
+	startTime int64
+
+	// worker fields, just to avoid reallocation of local vars
+	opCodeDuration int64
+	timerDuration  int64
+	log            InstrumenterLog
 }
 
 // NewInstrumenterLogger returns a new logger
@@ -44,16 +50,27 @@ func NewInstrumenterLogger(cfg *LogConfig) *InstrumenterLogger {
 
 // CaptureStart implements the Tracer interface to initialize the tracing operation.
 func (l *InstrumenterLogger) CaptureStart(from common.Address, to common.Address, create bool, input []byte, gas uint64, value *big.Int) error {
-	l.lastCaptureTime = runtimeNano()
+	l.startTime = runtimeNano()
 	return nil
 }
 
 // CaptureState logs a new structured log message and pushes it out to the environment
 func (l *InstrumenterLogger) CaptureState(env *vm.EVM, pc uint64, op vm.OpCode, gas, cost uint64, memory *vm.Memory, stack *vm.Stack, rStack *vm.ReturnStack, rData []byte, contract *vm.Contract, depth int, err error) error {
-	timeSincePrevious := runtimeNano() - l.lastCaptureTime
-	log := InstrumenterLog{pc, op, timeSincePrevious}
-	l.logs = append(l.logs, log)
-	l.lastCaptureTime = runtimeNano()
+	// measure the current iteration (we'll deduct startTime below)
+	l.opCodeDuration = runtimeNano()
+
+	// measure the most current timer overhead, take a new measurement and later deduct the
+	// previous timer reading
+	l.timerDuration = runtimeNano()
+	l.timerDuration -= l.opCodeDuration
+	l.opCodeDuration -= l.startTime
+
+	// add to log
+	l.log = InstrumenterLog{pc, op, l.opCodeDuration, l.timerDuration}
+	l.logs = append(l.logs, l.log)
+
+	// start timing the next iteration
+	l.startTime = runtimeNano()
 	return nil
 }
 
@@ -74,7 +91,7 @@ func (l *InstrumenterLogger) InstrumenterLogs() []InstrumenterLog { return l.log
 // WriteTrace writes a formatted trace to the given writer
 func WriteTrace(writer io.Writer, logs []InstrumenterLog) {
 	for _, log := range logs {
-		fmt.Fprintf(writer, "%-16spc=%08d time_ns=%v", log.Op, log.Pc, log.TimeNs)
+		fmt.Fprintf(writer, "%-16spc=%08d time_ns=%v timer_time_ns=%v", log.Op, log.Pc, log.TimeNs, log.TimerTimeNs)
 		fmt.Fprintln(writer)
 	}
 }
@@ -82,8 +99,7 @@ func WriteTrace(writer io.Writer, logs []InstrumenterLog) {
 func WriteCSVTrace(writer io.Writer, logs []InstrumenterLog, runId int) {
 	// CSV header must be in sync with these fields here :(, but it's in measurements.py
 	for instructionId, log := range logs {
-		// NOTE: we don't have measure_one_time_ns for now, we leave it out at the end
-		fmt.Fprintf(writer, "%v,%v,%v,", runId, instructionId, log.TimeNs)
+		fmt.Fprintf(writer, "%v,%v,%v,%v", runId, instructionId, log.TimeNs, log.TimerTimeNs)
 		fmt.Fprintln(writer)
 	}
 }
