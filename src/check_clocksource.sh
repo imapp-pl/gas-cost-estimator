49b5c227da57393902546258728a043fc9895191
pdobacz <5735525+pdobacz@users.noreply.github.com>
2021-10-01 17:02:17 +0200

    fix funny typo and order

--- src/check_clocksource.sh
+++ src/check_clocksource.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ `cat /sys/devices/system/clocksource/clocksource0/current_clocksource` != 'tc' ]; then
+if [ `cat /sys/devices/system/clocksource/clocksource0/current_clocksource` != 'tsc' ]; then
   echo "clocksource should be tsc, found:"
   cat /sys/devices/system/clocksource/clocksource0/current_clocksource
   echo "see docker_timer.md somewhere in the docses"
