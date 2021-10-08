614c27fb797283677b9d173d911755d05dc96484
pdobacz <5735525+pdobacz@users.noreply.github.com>
2021-10-08 14:25:57 +0200

    exit 1 on failed closource check

--- src/check_clocksource.sh
+++ src/check_clocksource.sh
@@ -5,4 +5,5 @@ if [ `cat /sys/devices/system/clocksource/clocksource0/current_clocksource` != '
   echo "clocksource should be tsc, found:"
   cat /sys/devices/system/clocksource/clocksource0/current_clocksource
   echo "see docker_timer.md somewhere in the docses"
+  exit 1
 fi
