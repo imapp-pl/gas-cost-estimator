119fb5bd6f1eabc650f5eb9bc22adf80fd940517
pdobacz <5735525+pdobacz@users.noreply.github.com>
2021-10-08 13:26:41 +0200

    Configure for cloud measurements - geth + clocksource fix (#54)
    * use relative paths for git submodules
    
    https://stackoverflow.com/questions/40841882/automatically-access-git-submodules-via-ssh-or-https
    
    * add basic .dockerignore
    
    * prevent go compile errors from evmc
    
    * some notes
    
    * solution for the runtimeNano problem on AWS
    
    * fix funny typo and order
    
    * fix clocksource solution in docs after review
    
    * rearrange Dockerfile lines for faster builds
    
    * rm downloaded tar for smaller image

---
+++ src/check_clocksource.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ `cat /sys/devices/system/clocksource/clocksource0/current_clocksource` != 'tsc' ]; then
+  echo "clocksource should be tsc, found:"
+  cat /sys/devices/system/clocksource/clocksource0/current_clocksource
+  echo "see docker_timer.md somewhere in the docses"
+fi
