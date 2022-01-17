4ed6e8d75f7f8e62555664fa5708040f48dc6602
Piotr Żelazko <piotr.zelazko@icloud.com>
2022-01-18 00:16:10 +0100

    feat: add simple nix shell as dev env

---
+++ shell.nix
@@ -0,0 +1,19 @@
+with import <nixpkgs> {};
+
+let
+  python-packages = p: (with p; [ fire ]);
+  python = pkgs.python39.withPackages python-packages;
+in
+mkShell {
+  buildInputs = [ python pkgs.go_1_17 ];
+
+  shellHook = ''
+    mkdir -p ".go/src/github.com/ethereum/"
+    if [ ! -f ".go/src/github.com/ethereum/" ] ; then
+      ln -s $(pwd)/../go-ethereum .go/src/github.com/ethereum/
+    fi
+
+    export GOPATH="$(pwd)/.go/"
+    export GO111MODULE=off
+  '';
+}
