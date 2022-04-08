3056d4f3176e5da11226ef2388815d322647a7d2
pdobacz <5735525+pdobacz@users.noreply.github.com>
2022-04-08 16:38:56 +0200

    Support PUSHx opcodes in marginal measurement program generation

--- src/program_generator/common.py
+++ src/program_generator/common.py
@@ -110,17 +110,17 @@ def _fill_opcodes_push_dup_swap(opcodes):
   dups = constants.EVM_DUPS
   swaps = constants.EVM_SWAPS
 
-  pushes = _opcodes_dict_push_dup_swap(pushes, [0] * len(pushes), [1] * len(pushes), parameter='00')
+  pushes = _opcodes_dict_push_dup_swap(pushes, [0] * len(pushes), [1] * len(pushes), ['03' * variant for variant in range(1,33)])
   opcodes = {**opcodes, **pushes}
   # For dups and swaps the removeds/addeds aren't precise. "removed" is how much is required to be on stack
   # so it must be pushed there once. "added" is how much is really added "extra"
-  dups = _opcodes_dict_push_dup_swap(dups, range(1, len(dups)), [1] * len(dups))
+  dups = _opcodes_dict_push_dup_swap(dups, range(1, len(dups)), [1] * len(dups), [None] * len(dups))
   opcodes = {**opcodes, **dups}
-  swaps = _opcodes_dict_push_dup_swap(swaps, range(2, len(swaps)+1), [0] * len(swaps))
+  swaps = _opcodes_dict_push_dup_swap(swaps, range(2, len(swaps)+1), [0] * len(swaps), [None] * len(swaps))
   opcodes = {**opcodes, **swaps}
   return opcodes
 
-def _opcodes_dict_push_dup_swap(source, removeds, addeds, parameter=None):
+def _opcodes_dict_push_dup_swap(source, removeds, addeds, parameters):
   source_list = source.split()
   opcodes = source_list[::2]
   names = source_list[1::2]
@@ -131,7 +131,7 @@ def _opcodes_dict_push_dup_swap(source, removeds, addeds, parameter=None):
       'Removed from stack': removed,
       'Added to stack': added,
       'Parameter': parameter
-    } for opcode, name, removed, added in zip(opcodes, names, removeds, addeds)
+    } for opcode, name, removed, added, parameter in zip(opcodes, names, removeds, addeds, parameters)
   }
 
   return new_part
