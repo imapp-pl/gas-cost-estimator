8eb92fc4531a37a5817a8a78aa435c7341698e07
lukasz-glen <>
2024-11-21 03:39:57 +0100

    fix: pg_arguments for CREATE clarification

--- src/program_generator/pg_arguments.py
+++ src/program_generator/pg_arguments.py
@@ -113,15 +113,23 @@ class ProgramGenerator(object):
     op_counts = [0, op_count, op_count * 2]
 
     if opcode == "CREATE":
-      opcode_arity = 3
+      # the opcode arity is 3
+      # allocate max possible used memory
       mem_allocation = "5f61080052"
-      initial_code_on_stack = "6c6460016001015f526005601bf360981b"
+      # put on stack the initial code for CREATE,
+      # the initial code on stack is the same as for marginal examination but shifted left for convenience
+      initial_code_on_stack = "7f6460016001015f526005601bf360000000000000000000000000000000000000"
+      # fill memory first 32 slots stores the same initial code
       initial_code_in_mem = ""
       for pre_offset in range(32):
         offset = "%0.4X" % (32 * pre_offset)
         initial_code_in_mem += "8061" + offset + "52"
+      # prefix code is the same for all programs - the same gas cost
       prefix_code = mem_allocation + initial_code_on_stack + initial_code_in_mem
       arg_sizes = [1, random.randint(1, 32), random.randint(1, 32)]
+      # prepare arguments, the range for the initial code passed to CREATE
+      # the offset and size are random, but effectively executed initial code is the same
+      # note that the effectively executed initial code and the deployed code are the same as for marginal examination
       single_op_pushes = ["61%0.4X" % (arg_sizes[2] * 32), "61%0.4X" % ((arg_sizes[1] - 1) * 32), "5f"]
       return [Program(prefix_code + generate_single_marginal(single_op_pushes, operation, o), operation['Mnemonic'], o, arg_sizes) for
               o in op_counts]
