329f8ea6c6bc8108b23e56ce331a5aa90e680001
Jacek Glen <JacekGlen@users.noreply.github.com>
2025-05-20 13:30:09 +0200

    Client refresh 2025-05 (#258)
    * Refresh clients
    
    * Update readme

--- scripts/setup_tools.sh
+++ scripts/setup_tools.sh
@@ -1,7 +1,8 @@
 # install .NET 7.0
+sudo add-apt-repository ppa:dotnet/backports
 wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
 chmod +x ./dotnet-install.sh
-sudo ./dotnet-install.sh --channel 7.0
+sudo ./dotnet-install.sh --channel 9.0
 rm ./dotnet-install.sh
 
 # install go
@@ -11,11 +12,11 @@ sudo snap install go --classic
 sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # install node
-sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
-sudo nvm install 20
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
+nvm install 20
 node -v
 npm -v
-sudo npm i -g @vercel/ncc 
+npm i -g @vercel/ncc 
 
 # install cmake
 sudo apt-get install cmake
